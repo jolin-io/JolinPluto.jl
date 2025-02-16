4e4981a2f91259218177d41cd3d752c46dca9c79
Stephan Sahm <stephan.sahm@jolin.io>
2025-02-16 17:57:41 +0100

    removing cell id magic from exports

--- src/JolinPluto.jl
+++ src/JolinPluto.jl
@@ -3,8 +3,7 @@ module JolinPluto
 export @repeat_take!, @repeat_at, @Channel
 export repeat_take!, repeat_take, repeat_at, ChannelPluto, repeat_queueget, start_python_thread, ChannelWithRepeatedFill, NoPut
 export output_below, clipboard_image_to_clipboard_html, embedLargeHTML
-export Setter, @get, @cell_ids_create_wrapper, @cell_ids_push!
-export cell_ids_create_wrapper, cell_ids_push!, cell_ids_push
+export Setter, @get
 export MD, format_html
 export viewof
 export IPyWidget, IPyWidget_init
